883aa384174e71902b3037f896bb937d3127cf6c
[kayhan-turker] <[kturker92@gmail.com]>
2021-07-02 18:31:54 -0400

    add map class

--- Main.py
+++ Main.py
@@ -1,24 +1,20 @@
-import math
-import colorsys
 import arcade
-import Particles
+from Particles import *
+from Mass import *
 
 MAX_X = 640
 MAX_Y = 640
 
-particles = Particles(MAX_X, MAX_Y)
-testTime = 0
-
 pNum = 6
-pRad = 128
-pX = [MAX_X / 2 + pRad * math.cos(i / pNum * 2 * math.pi) for i in range(pNum + 1)]
-pY = [MAX_Y / 2 + pRad * math.sin(i / pNum * 2 * math.pi) for i in range(pNum + 1)]
-pMag = [2, -2, 2, -2, 2, -2]
-pRad = [2 for i in range(pNum + 1)]
-pColor = [colorsys.hsv_to_rgb(i / pNum, 1.0, 255) for i in range(pNum + 1)]
+wallBounce = True
+
+particles = Particles(MAX_X, MAX_Y)
+masses = Mass(MAX_X, MAX_Y, pNum)
 
 mapColor = [[(0, 0, 0) for y in range(MAX_Y)] for x in range(MAX_X)]
 mapTime = [[0 for y in range(MAX_Y)] for x in range(MAX_X)]
+
+testTime = 0
 maxTime = 0
 minTime = 100000
 
@@ -26,12 +22,10 @@ maxTesting = (MAX_X - 1) * (MAX_Y - 1)
 numTesting = maxTesting
 numCollide = 0
 numOut = 0
-wallBounce = True
-
-drawParticles = 1200
-detail = max(1, round(math.sqrt(numTesting / maxTesting * numTesting / drawParticles)))
 
 toDrawMap = False
+drawParticles = 1200
+detail = max(1, round(math.sqrt(numTesting / maxTesting * numTesting / drawParticles)))
 
 
 class Canvas(arcade.Window):
@@ -47,7 +41,8 @@ class Canvas(arcade.Window):
         if toDrawMap:
             drawMap(arcade)
         else:
-            drawWorld(arcade)
+            particles.draw(arcade, detail)
+            masses.draw(arcade)
 
     def update(self, delta_time):
         mainLoop()
@@ -60,17 +55,6 @@ class Canvas(arcade.Window):
     def on_key_release(self, key, modifiers):
         if key == arcade.key.SPACE:
             pass
-
-
-def drawWorld(arc):
-    for tx in range(MAX_X):
-        for ty in range(MAX_Y):
-            if not testDone[tx][ty]:
-                if tx % detail == 0 and ty % detail == 0:
-                    arc.draw_point(testPosX[tx][ty], testPosY[tx][ty], (255, 255, 255), 1)
-
-    for p in range(pNum):
-        arc.draw_circle_filled(pX[p], pY[p], pRad[p], pColor[p])
 
 
 def drawMap(arc):
@@ -86,69 +70,29 @@ def drawMap(arc):
 
 
 def mainLoop():
-    global testPosX, testPosY, testVelX, testVelY, testTime
+    global testTime
 
-    for tx in range(MAX_X):
-        for ty in range(MAX_Y):
-            if not testDone[tx][ty]:
-                tpx = testPosX[tx][ty]
-                tpy = testPosY[tx][ty]
-                tvx = 0
-                tvy = 0
-
-                for p in range(pNum):
-                    disX = pX[p] - tpx
-                    disY = pY[p] - tpy
-                    dis = math.sqrt(disX * disX + disY * disY)
-                    if dis == 0:
-                        dis = 0.0001
-
-                    if dis < pRad[p]:
-                        endTest(tx, ty, pColor[p])
-                        break
-                    else:
-                        force = pMag[p] / dis / dis / dis
-                        tvx += force * disX
-                        tvy += force * disY
-
-                testVelX[tx][ty] += tvx
-                testVelY[tx][ty] += tvy
-                testPosX[tx][ty] += testVelX[tx][ty]
-                testPosY[tx][ty] += testVelY[tx][ty]
-
-                tpx = testPosX[tx][ty]
-                tpy = testPosY[tx][ty]
-
-                if wallBounce:
-                    if tpx < 0:
-                        testPosX[tx][ty] = -tpx
-                        testVelX[tx][ty] *= -1
-                    elif tpx > MAX_X:
-                        testPosX[tx][ty] = 2 * MAX_X - tpx
-                        testVelX[tx][ty] *= -1
-                    if tpy < 0:
-                        testPosY[tx][ty] = -tpx
-                        testVelY[tx][ty] *= -1
-                    elif tpy > MAX_Y:
-                        testPosY[tx][ty] = 2 * MAX_Y - tpy
-                        testVelY[tx][ty] *= -1
-                elif tpx < -MAX_X or tpx > MAX_X * 2 or tpy < -MAX_Y or tpy > MAX_Y * 2:
-                    endTest(tx, ty, (0, 0, 0), True)
+    doneList = particles.accelerate(masses, wallBounce)
+    if len(doneList) > 0:
+        for setP in doneList:
+            setMap(setP)
 
     testTime += 1
 
 
-def endTest(tx, ty, clr, out=False):
-    global testPosX, testPosY, testVelX, testVelY
+def setMap(particleData):
     global testTime, maxTime, minTime
     global numTesting, numCollide, numOut, detail
 
-    endTime = math.log(testTime + 1)
+    tx = particleData[0]
+    ty = particleData[1]
+    clr = particleData[2]
+    out = particleData[3]
 
     mapColor[tx][ty] = clr
-    mapTime[tx][ty] = endTime
-    testDone[tx][ty] = True
 
+    endTime = math.log(testTime + 1)
+    mapTime[tx][ty] = endTime
     if endTime > maxTime:
         maxTime = endTime
     if endTime < minTime:
@@ -165,7 +109,6 @@ def endTest(tx, ty, clr, out=False):
 
     if numTesting % 1000 == 0:
         detail = max(1, round(math.sqrt(numTesting / maxTesting * numTesting / drawParticles)))
-
 
 
 def main():
--- Particles.py
+++ Particles.py
@@ -1,13 +1,81 @@
+import math
+
 
 class Particles:
+    max_x, max_y = 0, 0
     px, py = [[]], [[]]
     vx, vy = [[]], [[]]
-    collided = [[]]
+    done = [[]]
 
-    def __init__(self, nx, ny):
-        self.px = [[x for y in range(ny)] for x in range(nx)]
-        self.py = [[y for y in range(ny)] for x in range(nx)]
-        self.vx = [[0 for y in range(ny)] for x in range(nx)]
-        self.vy = [[0 for y in range(ny)] for x in range(nx)]
-        self.collided = [[False for y in range(ny)] for x in range(nx)]
+    def __init__(self, max_x, max_y):
+        self.max_x = max_x
+        self.max_y = max_y
 
+        self.px = [[x for y in range(max_y)] for x in range(max_x)]
+        self.py = [[y for y in range(max_y)] for x in range(max_x)]
+        self.vx = [[0 for y in range(max_y)] for x in range(max_x)]
+        self.vy = [[0 for y in range(max_y)] for x in range(max_x)]
+        self.done = [[False for y in range(max_y)] for x in range(max_x)]
+
+    def draw(self, arc, detail):
+        for tx in range(self.max_x):
+            for ty in range(self.max_y):
+                if not self.done[tx][ty]:
+                    if tx % detail == 0 and ty % detail == 0:
+                        arc.draw_point(self.px[tx][ty], self.py[tx][ty], (255, 255, 255), 1)
+
+    def accelerate(self, masses, bounce):
+        doneList = []
+        max_x = self.max_x
+        max_y = self.max_y
+
+        for tx in range(max_x):
+            for ty in range(max_y):
+                if not self.done[tx][ty]:
+                    tpx = self.px[tx][ty]
+                    tpy = self.py[tx][ty]
+                    tvx = 0
+                    tvy = 0
+
+                    for p in range(masses.num):
+                        disX = masses.pX[p] - tpx
+                        disY = masses.pY[p] - tpy
+                        dis = math.sqrt(disX * disX + disY * disY)
+                        if dis == 0:
+                            dis = 0.0001
+
+                        if dis < masses.pRad[p]:
+                            doneList.append((tx, ty, masses.pColor[p], False))
+                            self.done[tx][ty] = True
+                            break
+                        else:
+                            force = masses.pMag[p] / dis / dis / dis
+                            tvx += force * disX
+                            tvy += force * disY
+
+                    self.vx[tx][ty] += tvx
+                    self.vy[tx][ty] += tvy
+                    self.px[tx][ty] += self.vx[tx][ty]
+                    self.py[tx][ty] += self.vy[tx][ty]
+
+                    tpx = self.px[tx][ty]
+                    tpy = self.py[tx][ty]
+
+                    if bounce:
+                        if tpx < 0:
+                            self.px[tx][ty] = -tpx
+                            self.vx[tx][ty] *= -1
+                        elif tpx > max_x:
+                            self.px[tx][ty] = 2 * max_x - tpx
+                            self.vx[tx][ty] *= -1
+                        if tpy < 0:
+                            self.py[tx][ty] = -tpx
+                            self.vy[tx][ty] *= -1
+                        elif tpy > max_y:
+                            self.py[tx][ty] = 2 * max_y - tpy
+                            self.vy[tx][ty] *= -1
+                    elif tpx < -max_x or tpx > max_x * 2 or tpy < -max_y or tpy > max_y * 2:
+                        doneList.append((tx, ty, (0, 0, 0), True))
+                        self.done[tx][ty] = True
+
+        return doneList
