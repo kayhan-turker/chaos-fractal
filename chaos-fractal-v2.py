759b0dc7dc2705c36441ea47dd632634fe404937
[kayhan-turker] <[kturker92@gmail.com]>
2021-07-07 10:26:01 -0400

    include second version - one test at a time

--- chaos-fractal-v2.py
+++ chaos-fractal-v2.py
@@ -1,33 +1,31 @@
 import math
 import colorsys
+import random
+
 import arcade
-import numpy
-from PIL import Image
 
 MAX_X = 512
 MAX_Y = 512
 
 defMass = 16
 defRad = 4
-numMass = 6
+numMass = 2
 ringRad = 128
 
-testPX = [[x for y in range(MAX_Y)] for x in range(MAX_X)]
-testPY = [[y for y in range(MAX_Y)] for x in range(MAX_X)]
-testVX = [[0 for y in range(MAX_Y)] for x in range(MAX_X)]
-testVY = [[0 for y in range(MAX_Y)] for x in range(MAX_X)]
-testRun = [[True for y in range(MAX_Y)] for x in range(MAX_X)]
+testSX = round(MAX_X / 2)
+testSY = round(MAX_Y / 2)
+
+testPX = testSX
+testPY = testSY
+testVX = 0
+testVY = 0
 testMag = defMass
 testRad = defRad
 
-massPX = [[[MAX_X / 2 + ringRad * math.cos(i / numMass * 2 * math.pi)
-            for y in range(MAX_Y)] for x in range(MAX_X)] for i in range(numMass + 1)]
-massPY = [[[MAX_Y / 2 + ringRad * math.sin(i / numMass * 2 * math.pi)
-            for y in range(MAX_Y)] for x in range(MAX_X)] for i in range(numMass + 1)]
-massVX = [[[0 for i in range(numMass + 1)
-            for y in range(MAX_Y)] for x in range(MAX_X)] for i in range(numMass + 1)]
-massVY = [[[0 for i in range(numMass + 1)
-            for y in range(MAX_Y)] for x in range(MAX_X)] for i in range(numMass + 1)]
+massPX = [MAX_X / 2 + ringRad * math.cos(i / numMass * 2 * math.pi) for i in range(numMass + 1)]
+massPY = [MAX_Y / 2 + ringRad * math.sin(i / numMass * 2 * math.pi) for i in range(numMass + 1)]
+massVX = [0 for i in range(numMass + 1)]
+massVY = [0 for i in range(numMass + 1)]
 massMag = [defMass * math.cos(math.pi * i) for i in range(numMass + 1)]
 massRad = [defRad for i in range(numMass + 1)]
 massClr = [colorsys.hsv_to_rgb(i / numMass, 1.0, 255) for i in range(numMass + 1)]
@@ -36,19 +34,14 @@ testTime = 0
 maxTime = 0
 minTime = 100000
 mapColor = [[(0, 0, 0) for y in range(MAX_Y)] for x in range(MAX_X)]
-mapTime = [[0.0 for y in range(MAX_Y)] for x in range(MAX_X)]
-
-mapArray = numpy.empty((MAX_Y, MAX_X, 3), dtype=numpy.uint8)
-mapImage = Image.fromarray(mapArray, 'RGB')
-
-testMax = MAX_X * MAX_Y
-testLeft = testMax
-testDone = 0
+mapTime = [[0 for y in range(MAX_Y)] for x in range(MAX_X)]
 
 wallBounce = False
 wallWrap = True
 circleWall = True
 wallRadius = min(MAX_X, MAX_Y) / 2
+
+toDrawMap = False
 
 
 class Canvas(arcade.Window):
@@ -61,198 +54,213 @@ class Canvas(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
-        drawWorld(arcade)
+        if toDrawMap:
+            drawMap(arcade)
+        else:
+            drawWorld(arcade)
 
     def update(self, delta_time):
         mainLoop()
 
     def on_key_press(self, key, modifiers):
+        global toDrawMap
         if key == arcade.key.SPACE:
-            getImage()
+            toDrawMap = not toDrawMap
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.SPACE:
+            pass
+
+
+def checkDone():
+    disX = testSX - MAX_X / 2
+    disY = testSY - MAX_Y / 2
+    dis = math.sqrt(disX * disX + disY * disY)
+    if dis > wallRadius:
+        endTest((0, 0, 0))
+
+
+def drawWorld(arc):
+    arc.draw_point(testPX, testPY, (255, 255, 255), testRad)
+    for p in range(numMass):
+        arc.draw_circle_filled(massPX[p], massPY[p], massRad[p], massClr[p])
+
+
+def drawMap(arc):
+    for mapX in range(MAX_X):
+        for mapY in range(MAX_Y):
+            if mapColor[mapX][mapY] != (0, 0, 0):
+                clr = mapColor[mapX][mapY]
+                time = mapTime[mapX][mapY]
+                factor = 1 - (time - minTime) / (maxTime - minTime) if maxTime != minTime else 1
+
+                clr = (clr[0] * factor, clr[1] * factor, clr[2] * factor)
+                arc.draw_point(mapX, mapY, clr, 1)
 
 
 def mainLoop():
-    global testTime, testLeft
+    global testTime
 
     accTest()
     accMass()
 
-    if testLeft > 0:
-        testTime += 1
-
-
-def checkDone():
-    for x in range(MAX_X):
-        for y in range(MAX_Y):
-            if circleWall:
-                disX = testPX[x][y] - MAX_X / 2
-                disY = testPY[x][y] - MAX_Y / 2
-                dis = math.sqrt(disX * disX + disY * disY)
-                if dis > wallRadius:
-                    endTest(x, y, (0, 0, 0))
-
-
-def getImage(display=True):
-    getMap()
-    mapImage.save('chaos-fractal-%i-%0.2f.png' % (numMass, testDone / (testDone + testLeft)))
-    if display:
-        mapImage.show()
-
-
-def drawWorld(arc):
-    if testLeft > 0:
-        for x in range(MAX_X):
-            for y in range(MAX_Y):
-                if testRun[x][y]:
-                    arc.draw_point(testPX[x][y], testPY[x][y], (255, 255, 255), testRad)
-                    for p in range(numMass):
-                        arc.draw_circle_filled(massPX[p][x][y], massPY[p][x][y], massRad[p], massClr[p])
-                    return
-
-
-def getMap():
-    global mapArray, mapImage
-
-    mapArray = numpy.empty((MAX_Y, MAX_X, 3), dtype=numpy.uint8)
-    for mapX in range(MAX_X):
-        for mapY in range(MAX_Y):
-            clr = mapColor[mapX][mapY]
-            time = mapTime[mapX][mapY]
-            factor = 1 - (time - minTime) / (maxTime - minTime) if maxTime != minTime else 1
-            clr = (clr[0] * factor, clr[1] * factor, clr[2] * factor)
-
-            mapArray[mapY, mapX] = [clr[0], clr[1], clr[2]]
-
-    mapImage = Image.fromarray(mapArray, 'RGB')
+    testTime += 1
 
 
 def accTest():
     global testPX, testPY, testVX, testVY
+    spx, spy, svx, svy = testPX, testPY, testVX, testVY
+    fpx, fpy, fvx, fvy = spx, spy, svx, svy
 
-    for x in range(MAX_X):
-        for y in range(MAX_Y):
-            if testRun[x][y]:
-                spx, spy, svx, svy = testPX[x][y], testPY[x][y], testVX[x][y], testVY[x][y]
-                fpx, fpy, fvx, fvy = spx, spy, svx, svy
+    for p in range(numMass):
+        disX = massPX[p] - spx
+        disY = massPY[p] - spy
+        dis = math.sqrt(disX * disX + disY * disY)
+        if dis == 0:
+            dis = 0.000001
 
-                for p in range(numMass):
-                    dvx, dvy = accelerate(spx, spy, massPX[p][x][y], massPY[p][x][y],
-                                          massRad[p] + testRad, massMag[p])
-                    if dvx == 0 and dvy == 0:
-                        endTest(x, y, massClr[p])
-                        break
-                    else:
-                        fvx += dvx
-                        fvy += dvy
+        if dis < massRad[p] + testRad:
+            endTest(massClr[p])
+            return
+        else:
+            force = massMag[p] / (dis * dis * dis)
+            fvx += force * disX
+            fvy += force * disY
 
-                fpx += fvx
-                fpy += fvy
+    fpx += fvx
+    fpy += fvy
 
-                testPX[x][y], testPY[x][y], testVX[x][y], testVY[x][y] = collide(fpx, fpy, fvx, fvy)
+    if wallBounce:
+        if fpx < 0:
+            fpx *= -1
+            fvx *= -1
+        elif fpx > MAX_X:
+            fpx = 2 * MAX_X - fpx
+            fvx *= -1
+        if fpy < 0:
+            fpy *= -1
+            fvy *= -1
+        elif fpy > MAX_Y:
+            fpy = 2 * MAX_Y - fpy
+            fvy *= -1
+    elif wallWrap:
+        if circleWall:
+            disX = fpx - MAX_X / 2
+            disY = fpy - MAX_Y / 2
+            dis = math.sqrt(disX * disX + disY * disY)
+            if dis > wallRadius:
+                fpx += -disX / dis * wallRadius * 2
+                fpy += -disY / dis * wallRadius * 2
+        else:
+            if fpx < 0:
+                fpx = MAX_X
+            elif fpx > MAX_X:
+                fpx = 0
+            if fpy < 0:
+                fpy = MAX_Y
+            elif fpy > MAX_Y:
+                fpy = 0
+
+    testPX, testPY, testVX, testVY = fpx, fpy, fvx, fvy
 
 
 def accMass():
-    for x in range(MAX_X):
-        for y in range(MAX_Y):
-            if testRun[x][y]:
-                for q in range(numMass):
-                    fpx, fpy, fvx, fvy = massPX[q][x][y], massPY[q][x][y], massVX[q][x][y], massVY[q][x][y]
+    for q in range(numMass):
+        fvx, fvy = massVX[q], massVY[q]
 
-                    radQ = massRad[q]
-                    for p in range(numMass):
-                        if p != q:
-                            dvx, dvy = accelerate(fpx, fpy, massPX[p][x][y], massPY[p][x][y],
-                                                  radQ + massRad[p], massMag[p])
-                            fvx += dvx
-                            fvy += dvy
+        radQ = massRad[q]
+        # mass to mass acceleration
+        for p in range(numMass):
+            if p != q:
+                disX = massPX[p] - massPX[q]
+                disY = massPY[p] - massPY[q]
+                dis = math.sqrt(disX * disX + disY * disY)
+                if dis == 0:
+                    dis = 0.000001
 
-                    dvx, dvy = accelerate(fpx, fpy, testPX[x][y], testPY[x][y],
-                                          radQ + testRad, testMag)
-                    fvx += dvx
-                    fvy += dvy
+                if dis > radQ + massRad[p]:
+                    force = massMag[p] / (dis * dis * dis)
+                    fvx += force * disX
+                    fvy += force * disY
 
-                    massVX[q][x][y], massVY[q][x][y] = fvx, fvy
+        # mass to test acceleration
+        disX = testPX - massPX[q]
+        disY = testPY - massPY[q]
+        dis = math.sqrt(disX * disX + disY * disY)
+        if dis == 0:
+            dis = 0.000001
 
-                for q in range(numMass):
-                    fpx, fpy, fvx, fvy = massPX[q][x][y], massPY[q][x][y], massVX[q][x][y], massVY[q][x][y]
+        if dis > radQ + testRad:
+            force = testMag / (dis * dis * dis)
+            fvx += force * disX
+            fvy += force * disY
 
-                    fpx += fvx
-                    fpy += fvy
+        # sum forces
+        massVX[q], massVY[q] = fvx, fvy
 
-                    massPX[q][x][y], massPY[q][x][y], massVX[q][x][y], massVY[q][x][y] = collide(fpx, fpy, fvx, fvy)
+    for q in range(numMass):
+        fpx, fpy = massPX[q], massPY[q]
 
+        fpx += massVX[q]
+        fpy += massVY[q]
 
-def accelerate(px, py, qx, qy, rad, mass):
-    disX = qx - px
-    disY = qy - py
-    dis = math.sqrt(disX * disX + disY * disY)
-    if dis == 0:
-        dis = 0.000001
-
-    if dis > rad:
-        force = mass / (dis * dis * dis)
-        return force * disX, force * disY
-    return 0, 0
-
-
-def collide(px, py, vx, vy):
-    if wallBounce:
-        if px < 0:
-            px *= -1
-            vx *= -1
-        elif px > MAX_X:
-            px = 2 * MAX_X - px
-            vx *= -1
-        if py < 0:
-            py *= -1
-            vy *= -1
-        elif py > MAX_Y:
-            py = 2 * MAX_Y - py
-            vy *= -1
-    elif wallWrap:
-        if circleWall:
-            disX = px - MAX_X / 2
-            disY = py - MAX_Y / 2
+        if wallBounce:
+            if fpx < 0:
+                fpx *= -1
+                massVX[q] *= -1
+            elif fpx > MAX_X:
+                fpx = 2 * MAX_X - fpx
+                massVX[q] *= -1
+            if fpy < 0:
+                fpy *= -1
+                massVY[q] *= -1
+            elif fpy > MAX_Y:
+                fpy = 2 * MAX_Y - fpy
+                massVY[q] *= -1
+        elif wallWrap:
+            disX = fpx - MAX_X / 2
+            disY = fpy - MAX_Y / 2
             dis = math.sqrt(disX * disX + disY * disY)
             if dis > wallRadius:
-                px += -disX / dis * wallRadius * 2
-                py += -disY / dis * wallRadius * 2
-        else:
-            if px < 0:
-                px = MAX_X
-            elif px > MAX_X:
-                px = 0
-            if py < 0:
-                py = MAX_Y
-            elif py > MAX_Y:
-                py = 0
+                fpx += -disX / dis * wallRadius * 2
+                fpy += -disY / dis * wallRadius * 2
 
-    return px, py, vx, vy
+        massPX[q], massPY[q] = fpx, fpy
 
 
-def endTest(x, y, clr):
+def endTest(clr):
+    global testSX, testSY
     global testPX, testPY, testVX, testVY
     global massPX, massPY, massVX, massVY
-    global testTime, maxTime, minTime, testDone, testLeft
+    global testTime, maxTime, minTime
 
-    mapColor[x][y] = clr
-    endTime = math.log(testTime + 1)
-    mapTime[x][y] = endTime
+    print(testSX, testSY, "DONE")
 
-    if endTime > maxTime:
-        maxTime = endTime
-    if endTime < minTime:
-        minTime = endTime
+    mapColor[testSX][testSY] = clr
+    testTime = math.log(testTime + 1)
+    mapTime[testSX][testSY] = testTime
 
-    testRun[x][y] = False
-    testLeft -= 1
-    testDone += 1
+    if testTime > maxTime:
+        maxTime = testTime
+    if testTime < minTime:
+        minTime = testTime
 
-    if testLeft % 100 == 0:
-        print(testLeft, testDone, round(testDone / (testDone + testLeft) * 100) / 100)
+    testTime = 0
 
-    if testLeft % 1000 == 0 and clr != (0, 0, 0):
-        getImage(False)
+    massPX = [MAX_X / 2 + ringRad * math.cos(i / numMass * 2 * math.pi) for i in range(numMass + 1)]
+    massPY = [MAX_Y / 2 + ringRad * math.sin(i / numMass * 2 * math.pi) for i in range(numMass + 1)]
+    massVX = [0 for i in range(numMass + 1)]
+    massVY = [0 for i in range(numMass + 1)]
+
+    testSX = random.randint(0, MAX_X)
+    testSY = random.randint(0, MAX_Y)
+
+    testPX = testSX
+    testPY = testSY
+    testVX = 0
+    testVY = 0
+
+    checkDone()
 
 
 def main():
---
+++ chaos-fractal-v3.py
@@ -0,0 +1,280 @@
+import math
+import colorsys
+import arcade
+import numpy
+from PIL import Image
+
+preview = True
+MAX_X = round(512 / (2 if preview else 1))
+MAX_Y = round(512 / (2 if preview else 1))
+
+defMass = 16
+defRad = 4
+negTest = False
+numMass = 3
+ringRad = 32
+alternateMass = False
+
+wallBounce = False
+wallWrap = False
+circleWall = False
+wallRadius = min(MAX_X, MAX_Y) / 2
+
+testPX = [[x for y in range(MAX_Y)] for x in range(MAX_X)]
+testPY = [[y for y in range(MAX_Y)] for x in range(MAX_X)]
+testVX = [[0 for y in range(MAX_Y)] for x in range(MAX_X)]
+testVY = [[0 for y in range(MAX_Y)] for x in range(MAX_X)]
+testRun = [[True for y in range(MAX_Y)] for x in range(MAX_X)]
+testMag = defMass * (-1 if negTest else 1)
+testRad = defRad
+
+massPX = [[[MAX_X / 2 + ringRad * math.cos(i / numMass * 2 * math.pi)
+            for y in range(MAX_Y)] for x in range(MAX_X)] for i in range(numMass + 1)]
+massPY = [[[MAX_Y / 2 + ringRad * math.sin(i / numMass * 2 * math.pi)
+            for y in range(MAX_Y)] for x in range(MAX_X)] for i in range(numMass + 1)]
+massVX = [[[0 for i in range(numMass + 1)
+            for y in range(MAX_Y)] for x in range(MAX_X)] for i in range(numMass + 1)]
+massVY = [[[0 for i in range(numMass + 1)
+            for y in range(MAX_Y)] for x in range(MAX_X)] for i in range(numMass + 1)]
+massMag = [defMass * math.cos((1 if alternateMass else 2) * math.pi * i) for i in range(numMass + 1)]
+massRad = [defRad for i in range(numMass + 1)]
+massClr = [colorsys.hsv_to_rgb(i / numMass, 1.0, 255) for i in range(numMass + 1)]
+
+testTime = 0
+maxTime = 0
+minTime = 100000
+mapColor = [[(0, 0, 0) for y in range(MAX_Y)] for x in range(MAX_X)]
+mapTime = [[0.0 for y in range(MAX_Y)] for x in range(MAX_X)]
+
+mapArray = numpy.empty((MAX_Y, MAX_X, 3), dtype=numpy.uint8)
+mapImage = Image.fromarray(mapArray, 'RGB')
+
+testMax = MAX_X * MAX_Y
+testLeft = testMax
+testDone = 0
+
+
+class Canvas(arcade.Window):
+    def __init__(self):
+        super().__init__(MAX_X, MAX_Y, "Chart")
+        arcade.set_background_color((0, 0, 0))
+
+    def setup(self):
+        if preview:
+            halfDimensions()
+        checkDone()
+
+    def on_draw(self):
+        arcade.start_render()
+        drawWorld(arcade)
+
+    def update(self, delta_time):
+        mainLoop()
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.SPACE:
+            getImage()
+
+
+def mainLoop():
+    global testTime, testLeft
+
+    accTest()
+    accMass()
+
+    if testLeft > 0:
+        testTime += 1
+
+
+def halfDimensions():
+    global ringRad, wallRadius, testMag, testRad, massMag, massRad
+    ringRad = round(ringRad / 2)
+    wallRadius = round(wallRadius / 2)
+    testRad = round(testRad / 2)
+    for p in range(numMass):
+        massRad[p] = round(massRad[p] / 2)
+
+
+def checkDone():
+    for x in range(MAX_X):
+        for y in range(MAX_Y):
+            if circleWall:
+                disX = testPX[x][y] - MAX_X / 2
+                disY = testPY[x][y] - MAX_Y / 2
+                dis = math.sqrt(disX * disX + disY * disY)
+                if dis > wallRadius:
+                    endTest(x, y, (0, 0, 0))
+
+
+def drawWorld(arc):
+    if testLeft > 0:
+        for x in range(MAX_X):
+            for y in range(MAX_Y):
+                if testRun[x][y]:
+                    arc.draw_circle_filled(testPX[x][y], testPY[x][y], testRad, (255, 255, 255))
+                    for p in range(numMass):
+                        arc.draw_circle_filled(massPX[p][x][y], massPY[p][x][y], massRad[p], massClr[p])
+                    return
+
+
+def getImage(display=True):
+    getMap()
+    if not preview:
+        mapImage.save('chaos-fractal-%i-%0.2f.png' % (numMass, testDone / (testDone + testLeft)))
+    if display:
+        mapImage.show()
+
+
+def getMap():
+    global mapArray, mapImage
+
+    mapArray = numpy.empty((MAX_Y, MAX_X, 3), dtype=numpy.uint8)
+    for mapX in range(MAX_X):
+        for mapY in range(MAX_Y):
+            clr = mapColor[mapX][mapY]
+            time = mapTime[mapX][mapY]
+            factor = 1 - (time - minTime) / (maxTime - minTime) if maxTime != minTime else 1
+            clr = (clr[0] * factor, clr[1] * factor, clr[2] * factor)
+
+            mapArray[MAX_Y - mapY - 1, mapX] = [clr[0], clr[1], clr[2]]
+
+    mapImage = Image.fromarray(mapArray, 'RGB')
+
+
+def accTest():
+    global testPX, testPY, testVX, testVY
+
+    for x in range(MAX_X):
+        for y in range(MAX_Y):
+            if testRun[x][y]:
+                spx, spy, svx, svy = testPX[x][y], testPY[x][y], testVX[x][y], testVY[x][y]
+                fpx, fpy, fvx, fvy = spx, spy, svx, svy
+
+                for p in range(numMass):
+                    dvx, dvy = accelerate(spx, spy, massPX[p][x][y], massPY[p][x][y],
+                                          massRad[p] + testRad, massMag[p])
+                    if dvx == 0 and dvy == 0:
+                        endTest(x, y, massClr[p])
+                        break
+                    else:
+                        fvx += dvx
+                        fvy += dvy
+
+                fpx += fvx
+                fpy += fvy
+
+                testPX[x][y], testPY[x][y], testVX[x][y], testVY[x][y] = collide(fpx, fpy, fvx, fvy)
+
+
+def accMass():
+    for x in range(MAX_X):
+        for y in range(MAX_Y):
+            if testRun[x][y]:
+                for q in range(numMass):
+                    fpx, fpy, fvx, fvy = massPX[q][x][y], massPY[q][x][y], massVX[q][x][y], massVY[q][x][y]
+
+                    radQ = massRad[q]
+                    for p in range(numMass):
+                        if p != q:
+                            dvx, dvy = accelerate(fpx, fpy, massPX[p][x][y], massPY[p][x][y],
+                                                  radQ + massRad[p], massMag[p])
+                            fvx += dvx
+                            fvy += dvy
+
+                    dvx, dvy = accelerate(fpx, fpy, testPX[x][y], testPY[x][y],
+                                          radQ + testRad, testMag)
+                    fvx += dvx
+                    fvy += dvy
+
+                    massVX[q][x][y], massVY[q][x][y] = fvx, fvy
+
+                for q in range(numMass):
+                    fpx, fpy, fvx, fvy = massPX[q][x][y], massPY[q][x][y], massVX[q][x][y], massVY[q][x][y]
+
+                    fpx += fvx
+                    fpy += fvy
+
+                    massPX[q][x][y], massPY[q][x][y], massVX[q][x][y], massVY[q][x][y] = collide(fpx, fpy, fvx, fvy)
+
+
+def accelerate(px, py, qx, qy, rad, mass):
+    disX = qx - px
+    disY = qy - py
+    dis = math.sqrt(disX * disX + disY * disY)
+    if dis == 0:
+        dis = 0.000001
+
+    if dis > rad:
+        force = mass / (dis * dis * dis)
+        return force * disX, force * disY
+    return 0, 0
+
+
+def collide(px, py, vx, vy):
+    if wallBounce:
+        if px < 0:
+            px *= -1
+            vx *= -1
+        elif px > MAX_X:
+            px = 2 * MAX_X - px
+            vx *= -1
+        if py < 0:
+            py *= -1
+            vy *= -1
+        elif py > MAX_Y:
+            py = 2 * MAX_Y - py
+            vy *= -1
+    elif wallWrap:
+        if circleWall:
+            disX = px - MAX_X / 2
+            disY = py - MAX_Y / 2
+            dis = math.sqrt(disX * disX + disY * disY)
+            if dis > wallRadius:
+                px += -disX / dis * wallRadius * 2
+                py += -disY / dis * wallRadius * 2
+        else:
+            if px < 0:
+                px = MAX_X
+            elif px > MAX_X:
+                px = 0
+            if py < 0:
+                py = MAX_Y
+            elif py > MAX_Y:
+                py = 0
+
+    return px, py, vx, vy
+
+
+def endTest(x, y, clr):
+    global testPX, testPY, testVX, testVY
+    global massPX, massPY, massVX, massVY
+    global testTime, maxTime, minTime, testDone, testLeft
+
+    mapColor[x][y] = clr
+    endTime = math.log(testTime + 1)
+    mapTime[x][y] = endTime
+
+    if endTime > maxTime:
+        maxTime = endTime
+    if endTime < minTime:
+        minTime = endTime
+
+    testRun[x][y] = False
+    testLeft -= 1
+    testDone += 1
+
+    if testLeft % 100 == 0:
+        print(testLeft, testDone, round(testDone / (testDone + testLeft) * 100) / 100)
+
+    if testLeft % 1000 == 0 and clr != (0, 0, 0):
+        getImage(False)
+
+
+def main():
+    canv = Canvas()
+    canv.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
